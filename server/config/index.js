c844289e2dbe2c2937df6f13988d6cfaee58e807
Bram van der Giessen <bgiessen@nordija.com>
2021-05-20 23:31:01 +0200

    Renamed paths.js to .cjs extension to enable CommonJs

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,7 @@
+import pkg from './paths.cjs';
+const { pathConfig } = pkg;
 // Nice to keep all exports in one place
 // so we can import them all from the same file in other places
 export { appConfig } from './app.js'
 export { routeConfig } from './routes.js'
-export { pathConfig } from './paths.cjs'
+export { pathConfig }
--- server/config/paths.cjs
+++ server/config/paths.cjs
@@ -4,7 +4,7 @@ import path from 'path'
 //=====================================
 //  PATHS CONFIGURATION
 //-------------------------------------
-export const pathConfig = {
+export default {
 	indexHtml: path.resolve ( __dirname, '../../build/index.html' ),
 	static: path.resolve ( __dirname, '../../build' )
 }
