7228ee3b8df19ab7d860b07fe4611626cab478ae
Bram van der Giessen <bgiessen@nordija.com>
2021-05-20 23:56:40 +0200

    Renamed paths.js to .cjs extension to enable CommonJs

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,6 @@
-import pathConfig from './paths.cjs';
+import _pathConfig from './paths.cjs';
 // Nice to keep all exports in one place
 // so we can import them all from the same file in other places
 export { appConfig } from './app.js'
 export { routeConfig } from './routes.js'
-export const pathConfig = pathConfig;
-console.log(pathConfig);
+export const pathConfig = _pathConfig;
